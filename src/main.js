3bc1ba24a9e697bd6125daeff868f02b58d775d7
langlang.chen <langlang.chen@advantech.com.cn>
2020-11-04 23:57:16 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import './plugins/axios'
-import './plugins/fontawesome'
-import './plugins/element.js'
-import App from './App.vue'
-import router from './routes'
-import store from './store'
+import Vue from "vue";
+import "./plugins/axios";
+import "./plugins/fontawesome";
+import "./plugins/element.js";
+import App from "./App.vue";
+import router from "./routes";
+import store from "./store";
 
-import AudioPlayer from "./../../92xx_vue-audio-player/src/audio-player/index";
+import AudioPlayer from "@langnang/vue-audio-player";
 
 Vue.use(AudioPlayer);
 
-Vue.config.productionTip = false
-Vue.use(require('vue-wechat-title'))
+Vue.config.productionTip = false;
+Vue.use(require("vue-wechat-title"));
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
